2631546cec26cebd33127b4acad7e28c531e0917
Taiyeb Radiowala <toy24052003@gmail.com>
2024-03-01 19:34:58 +1100

    fixed 2 errros

--- src/BookTestPage.tsx
+++ src/BookTestPage.tsx
@@ -1,45 +1,3 @@
-// import HTMLFlipBook from 'react-pageflip';
-// import React from 'react';
-
-// const Page = React.forwardRef((props: { number, children }, ref) => (
-//   <div className="demoPage bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" ref={ref}>
-//       <h1 className="text-xl font-bold mb-2">Page Header</h1>
-//       <p className="text-base">{props.children}</p>
-//       <p className="text-base">Page number: {props.number}</p>
-//   </div>
-// ));
-
-// const Book = () => {
-//   // Create refs for each page
-//   const pageRefs = [React.useRef(null), React.useRef(null), React.useRef(null), React.useRef(null)];
-
-//   return (
-//     <HTMLFlipBook width={300}
-//       height={500} className={''}
-//       flippingTime={10}
-//       >
-
-//       {pageRefs.map((ref, index) => (
-//         <Page key={index} number={`${index + 1}`} ref={ref}>Page text</Page>
-//       ))}
-//     </HTMLFlipBook>
-//   );
-// };
-
-// const BookTestPage = (props: {}) => {
-//   return (
-//     <div className="h-screen w-screen bg-neutral-100 flex justify-center items-center">
-//       <div className="w-[900px] h-[600px]">
-//         <Book />
-//       </div>
-//     </div>
-//   )
-
-// }
-
-
-// export default BookTestPage 
-
 
 import React from 'react';
 import HTMLFlipBook from 'react-pageflip';
@@ -57,7 +15,7 @@ const Book = () => {
   const pageRefs = [React.useRef<HTMLDivElement>(null), React.useRef<HTMLDivElement>(null), React.useRef<HTMLDivElement>(null), React.useRef<HTMLDivElement>(null)];
 
   return (
-    <HTMLFlipBook width={300} height={500} flippingTime={10}>
+    <HTMLFlipBook width={400} height={600} flippingTime={10}>
       {pageRefs.map((ref, index) => (
         <Page key={index} number={index + 1} ref={ref}>Page text</Page>
       ))}
@@ -68,7 +26,7 @@ const Book = () => {
 const BookTestPage = () => {
   return (
     <div className="h-screen w-screen bg-neutral-100 flex justify-center items-center">
-      <div className="w-[900px] h-[600px]">
+      <div className="w-[800px] h-[580px] justify-center">
         <Book />
       </div>
     </div>
