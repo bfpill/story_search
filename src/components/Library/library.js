b17ed0dc6db9fd1e968dd8f598afde3683fa0bc8
Taiyeb Radiowala <toy24052003@gmail.com>
2024-03-01 22:08:55 +1100

    deleted login

---
+++ src/components/Library/library.js
@@ -0,0 +1,29 @@
+import "./login.css";
+import "../../webroot/vendor/bootstrap/css/bootstrap.min.css";
+import "../../webroot/vendor/animate/animate.css";
+import "../../webroot/fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+import "../../webroot/vendor/animsition/css/animsition.min.css";
+import "../../webroot/fonts/iconic/css/material-design-iconic-font.min.css";
+import "../../webroot/vendor/select2/select2.min.css";
+import "../../webroot/vendor/daterangepicker/daterangepicker.css";
+import "../../util.css";
+import "../../main.css";
+import {
+  useNavigate,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import axios from 'axios';
+import React, { useState } from 'react';
+
+
+function Library(){
+
+    return (
+        <>
+            HIjpoaeaopjfoqegpoqq3fjp
+        
+        
+        </>
+    )
+}
+export default Library;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-
+import Library from './components/Library/library.js'
 import { useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home.tsx';
@@ -29,6 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='library' element={<Library />} />
       </Routes>
     </Router>
   </UserProvider>
