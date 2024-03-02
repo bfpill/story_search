eae3a7af3fd200163dd95d20bced48ac660771c8
Taiyeb Radiowala <toy24052003@gmail.com>
2024-03-02 22:02:25 +1100

    added the cover pages with coour and image

--- src/components/BookTitlePage.tsx
+++ src/components/BookTitlePage.tsx
@@ -1,16 +1,34 @@
-const BookTitlePage = (props: {complementaryColor, page}) => {
-  return (
-    <div className="h-full px-8 pt-6 pb-8 mb-4 bookcover"
-      style={{
-        backgroundColor: props.complementaryColor
 
-      }}>
-      <div className="h-1/3 bg-blue" />
-      <div className="h-min relative text-center select-none">
-        {props.page.text}
+import React, { useState, useEffect } from 'react';
+import image1 from '../assets/book_covers/img1.jpg';
+import image3 from '../assets/book_covers/img3.jpg';
+
+import image5 from '../assets/book_covers/img5.jpg';
+import image6 from '../assets/book_covers/img6.jpg';
+
+const BookTitlePage = (props: { complementaryColor, page }) => {
+  const [coverImage, setCoverImage] = useState('');
+  const [overlayColor, setOverlayColor] = useState('');
+
+  useEffect(() => {
+    const images = [image1, image3,  image5, image6];
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    setCoverImage(randomImage);
+
+    // Generate random RGB color for the overlay
+    const randomColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.6)`;
+    setOverlayColor(randomColor);
+  }, []);
+
+  return (
+    <div className="h-full px-8 pt-6 pb-8 mb-4 flex items-center justify-center bookcover relative">
+      <img src={coverImage} alt="Book Cover" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0" />
+      <div className="absolute top-0 left-0 w-full h-full z-10" style={{ backgroundColor: overlayColor }}></div>
+      <div className="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+        <div className="text-4xl font-bold">{props.page.text}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BookTitlePage
+export default BookTitlePage;
