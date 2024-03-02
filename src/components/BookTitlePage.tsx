90df205cb663d56823462813fd7c8f010ed450d0
maiasullivan <s3942603@student.rmit.edu.au>
2024-03-03 10:30:05 +1100

    font

--- src/components/BookTitlePage.tsx
+++ src/components/BookTitlePage.tsx
@@ -23,7 +23,7 @@ const BookTitlePage = (props) => {
       <img src={selectedCoverImage} alt="Book Cover" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0" />
       <div className="absolute top-0 left-0 w-full h-full z-10" style={{ backgroundColor: overlayColor }}></div>
       <div className="text-center absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
-      <div className={`transition-opacity duration-2000 text-3xl font-bold`} style={{ "opacity": textVisible ? 0.7 : 0, "color": "white", "textShadow": "0 0 4px rgba(0, 0, 0, 0.8)" }}>
+      <div className={`transition-opacity duration-2000 text font-bold`} style={{ "opacity": textVisible ? 0.7 : 0, "color": "white", "textShadow": "0 0 4px rgba(0, 0, 0, 0.8)" }}>
           {props.page.text}
         </div>
       </div>
